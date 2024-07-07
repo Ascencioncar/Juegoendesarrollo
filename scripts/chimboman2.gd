86dd1d4c7fa53717588c83b181c24dcdb4a8ccc7
Ascencioncar <carlosrp1918@gmail.com>
2024-07-07 15:58:44 -0500

    proyecto chimbo man

---
+++ scripts/chimboman2.gd
@@ -0,0 +1,67 @@
+extends CharacterBody2D
+
+var velocidad : int = 200
+var salto : int = 250
+var gravedad : int = 400
+var hitplayer = false
+var cont_jump : int = 0
+var max_jump : int = 2
+
+@onready var anim = $Anim2
+@onready var collisionplayer = $Collisionplayer2
+
+# Ruta al nuevo personaje que reemplazará al actual
+var new_character_path = "res://scenes/chimboman.tscn"
+
+func _physics_process(delta):
+	velocity.y += gravedad * delta
+	if !hitplayer:
+		if Input.is_action_pressed("ui_right"):
+			velocity.x = velocidad
+			anim.flip_h = false
+		elif Input.is_action_pressed("ui_left"):
+			velocity.x = -velocidad
+			anim.flip_h = true
+		else:
+			velocity.x = 0
+
+		#if is_on_floor():
+			#cont_jump = 0
+			#if Input.is_action_just_pressed("saltar"):
+				#velocity.y = -salto
+
+		animaciones()
+
+	move_and_slide()
+
+func animaciones():
+	if is_on_floor():
+		if velocity.x != 0:
+			anim.play("mueve + hide")
+		else:
+			anim.play("idle + hide ")
+
+func _input(_event):
+	if Input.is_action_just_pressed("agachar") and !hitplayer:
+		set_physics_process(false)
+		anim.play("up")
+		await anim.animation_finished
+		replace_character()
+
+func replace_character():
+ 	# Cargar la nueva escena del personaje
+	var new_character_scene = load(new_character_path)
+	var new_character = new_character_scene.instantiate()
+
+	# Obtener el nodo padre del personaje actual
+	var parent = get_parent()
+
+	# Transferir las propiedades físicas y posición
+	new_character.velocity = self.velocity
+	new_character.global_position = self.global_position
+
+	# Agregar el nuevo personaje al nodo padre
+	parent.add_child(new_character)
+
+	# Remover el personaje actual de la escena
+	queue_free()
